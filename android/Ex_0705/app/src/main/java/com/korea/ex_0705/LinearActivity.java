1c1bd4bf723c052b2c756011b1cdf80e100e3b17
kim sangjae <kimsangjae94114@gmail.com>
2023-07-06 17:41:47 +0900

    0706 안드로이드 레이아웃

---
+++ android/Ex_0705/app/src/main/java/com/korea/ex_0705/LinearActivity.java
@@ -0,0 +1,15 @@
+package com.korea.ex_0705;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LinearActivity extends AppCompatActivity  {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear);
+    }
+}
